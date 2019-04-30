393ef8a4037f06c997eee8d163884cebb60c196d
JongKeun Choi <jkchoi@nexell.co.kr>
2019-04-30 12:52:05 +0900

    Add vip_test application
    This patch is a test application that can test VIP, Deinterlacer and
    DRM Display.
    The usage is as follows.
    
     # vip_test -h
     Usage : vip_test [options]
      common options :
         -s [width],[height]         : input image's size
         -c [crop_width],[crop_height]    : crop image's size
         -d [display_width],[display_height] : display size
         -o [output file name]     : output file name
             The storage path is "/data/"
         -D : Enable deinterlacer
         -P : Enable preview
         -E : Enable rate adjust
         -h : help
     ==========================================================
     Examples
      camera display :
         #> vip_test camera_test -s 720,480 -D -P
         #> vip_test camera_test -s 720,480 -D -P -E
         #> vip_test camera_test -s 960,480 -c 720,480 -d 720,480 \
     		-o output.yuv -D -P
    
    Signed-off-by: JongKeun Choi <jkchoi@nexell.co.kr>
    Change-Id: If4e843218f8277ca7d6404aa3079c92b03df2210

---
+++ src/Util.h
@@ -0,0 +1,42 @@
+#ifndef __UTIL_h__
+#define __UTIL_h__
+
+#include <stdint.h>
+
+/* Application Data */
+typedef struct APP_DATA {
+	int32_t width;				/* Input YUV Image Width */
+	int32_t height;				/* Input YUV Image Height */
+	int32_t cropWidth;			/* Crop YUV Image Width */
+	int32_t cropHeight;			/* Crop YUV Image Height */
+	int32_t dpWidth;			/* Display YUV Image Width */
+	int32_t dpHeight;			/* Display YUV Image Height */
+
+	int32_t dpPort;
+
+	bool bEnableDeinter;
+	bool bEnablePreview;
+	bool bEnableRatio;
+
+	/* Output Options */
+	char *outFileName;			/* Output File Name */
+} APP_DATA;
+
+uint64_t NX_GetTickCount( void );
+void NX_DumpData( void *data, int32_t len, const char *pFormat, ... );
+void NX_DumpStream( uint8_t *pStrmBuf, int32_t iStrmSize, const char *pFormat, ... );
+void NX_DumpStream( uint8_t *pStrmBuf, int32_t iStrmSize, FILE *pFile );
+
+
+typedef struct MP_DRM_PLANE_INFO {
+	int32_t		iConnectorID;		//  Dsp Connector ID
+	int32_t		iPlaneId;			//  DRM Plane ID
+	int32_t		iCrtcId;			//  DRM CRTC ID
+} MP_DRM_PLANE_INFO;
+
+int32_t NX_FindPlaneForDisplay(int32_t crtcIdx, int32_t findRgb,
+				int32_t layerIdx,
+				MP_DRM_PLANE_INFO *pDrmPlaneInfo);
+int32_t NX_GetDisplayResolution(uint16_t *width, uint16_t *height);
+
+#endif // __UTIL_h__
